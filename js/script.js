88ca7a191de11e302179bc8d2ce6c3dad3081081
Gabriel Enriquez <gkikez97@gmail.com>
2023-05-04 21:27:59 -0400

    js file

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var myVar;
+
+function myFunction() {
+  myVar = setTimeout(showPage, 3000);
+}
+
+function showPage() {
+  document.getElementById("loader").style.display = "none";
+  document.getElementById("myDiv").style.display = "block";
+}
+
+function message (){
+  console.log("Hello, Welcome to my console logs.");
+}
+
+message();
